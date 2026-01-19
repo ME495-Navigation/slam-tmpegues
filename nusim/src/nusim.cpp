d85eb0a40b6d9b2269c75b4d0e6546e4c9508c5e
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-01-19 17:28:41 -0600

    Created base nusim node with timer.
    CMakeLists.txt:
    >  Uncommented nusim node lines
    
    nusim.launch.xml:
    >  Uncommented nusim node lines
    
    nusim.cpp (the node itself):
    +  Created timer with `rate` parameter. Timer info logs "Tick Tock" to
    ++ show that it's running
    
    Next steps: Make the publisher for the wall markers

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,13 +1,38 @@
 
 
-// Create parameter 'rate', default 100 Hz
-// Create ~/reset service, type std/srv/Empty
+#include "rclcpp/rclcpp.hpp"
+#include "std_srvs/srv/empty.hpp"
+
+using Empty = std_srvs::srv::Empty;
+
+class nusim_node : public rclcpp::Node
+{
+public:
+    nusim_node()
+    : Node("nusim")
+    {
+    // Create parameter 'rate', default 100 Hz
+    this->declare_parameter("rate", 100); // TODO: check removing the this->
+    int rate = this->get_parameter("rate").as_int();
+
+    auto timer_callback = [this]() -> void
+        { // TODO: Check removing the -> void, moving the whole lambda
+        RCLCPP_INFO(this->get_logger(), "Tick Tock"); // TODO: check using RCLCPP_DEBUG_STREAM
+        };
+    auto timer_period {std::chrono::milliseconds(1000/rate)};
+    timer_ = this->create_wall_timer(timer_period, timer_callback);
+    // Create ~/reset service, type std/srv/Empty
     // Resets simulation
-        // Right now, just resets the ~/timestep to 0
-        // At next task, the position of the robot should be reset to x0, y0, theta0
-            // Read the parameters at that time, not the values at initialization
+    // Right now, just resets the ~/timestep to 0
+    // At next task, the position of the robot should be reset to x0, y0, theta0
+    // Read the parameters at that time, not the values at initialization
+    }
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
 
+};
 
+std::shared_ptr<nusim_node> my_node = nullptr;
 
 // Create parameters x0, y0, theta0 for initial pose of red turtle
     // default all to 0.0
@@ -31,3 +56,14 @@
     // All obstacles are red
     // Publish visualization_msgs/MarkerArray on ~/real_obstacles
         // PUblish in the red namespace of Marker message
+
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    my_node = std::make_shared<nusim_node>();
+    // auto toggle_service = my_node->create_service<Empty>("toggle", toggle_cb);
+    rclcpp::spin(my_node);
+    rclcpp::shutdown();
+    return 0;
+}
