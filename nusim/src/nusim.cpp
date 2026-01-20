0f67a6f4bf02e8dae6a78017a840c64a50bd5c27
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-01-19 22:42:59 -0600

    DNC attempting to add marker

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,9 +1,13 @@
 
 
+#include "geometry_msgs/msg/point.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_srvs/srv/empty.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "visualization_msgs/msg/marker_array.hpp"
 
 using Empty = std_srvs::srv::Empty;
+using MarkerArray = visualization_msgs::msg::MarkerArray;
 
 class nusim_node : public rclcpp::Node
 {
@@ -26,10 +30,40 @@ public:
     // Right now, just resets the ~/timestep to 0
     // At next task, the position of the robot should be reset to x0, y0, theta0
     // Read the parameters at that time, not the values at initialization
+
+    // Walls
+    // Create parameters arena_x_length, arena_y_length
+    this->declare_parameter("arena_x_length", 10); // TODO: check removing the this->
+    double arena_x_length = this->get_parameter("arena_x_length").as_double();
+    this->declare_parameter("arena_y_length", 10); // TODO: check removing the this->
+    double arena_y_length = this->get_parameter("arena_y_length").as_double();
+
+    // Center arena at 0,0
+    // Walls 0.25 tall
+    // Walls are red
+    // use visualization_msgs/MarkerArray on topic ~/real_walls
+        // Check qos from assignment 2
+    rclcpp::QoS marker_qos(10);
+    marker_qos.durability(rclcpp::DurabilityPolicy::TransientLocal);
+    auto wall_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("~/real_walls", marker_qos);
     }
+
 private:
     rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<MarkerArray>::SharedPtr wall_pub_;
 
+    // The following section creates a wall marker
+    double wall_thick{0.01};
+    double wall_height{0.25};
+    visualization_msgs::msg::Marker(0) marker;
+    marker.header.frame_id = "~/world";
+    marker.header.stamp = rclcpp::Clock().now();
+    marker.id = 0;
+    marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    marker.color.r = 1.0;
+    marker.a = 0.75;
+
+    // geometry_msgs::msg::Point p1{x : arena_x_length, y : arena_y_length, z : wall_height / 2.0};
 };
 
 std::shared_ptr<nusim_node> my_node = nullptr;
@@ -38,13 +72,6 @@ std::shared_ptr<nusim_node> my_node = nullptr;
     // default all to 0.0
     // relative to nusim/world frame
 
-// Walls
-    // Create parameters arena_x_length, arena_y_length
-    // Center arena at 0,0
-    // Walls 0.25 tall
-    // Walls are red
-    // use visualization_msgs/MarkerArray on topic ~/real_walls
-        // Check qos from assignment 2
 
 // Obstacles
     // Not specified how to get this to work
