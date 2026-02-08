0dcf6037afd95727d5abfa187950703a01aa96eb
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-02-08 17:17:29 -0600

    Beginning odometry state and joint_state_cb_

--- nuturtle_control/src/odometry.cpp
+++ nuturtle_control/src/odometry.cpp
@@ -1,4 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 
 class odometry : public rclcpp::Node
@@ -12,8 +14,11 @@ public:
     this->declare_parameter<std::string>("wheel_left");
     this->declare_parameter<std::string>("right_left");
 
+
+
     body_id = this->get_parameter("body_id").as_string();
     odom_id = this->get_parameter("odom_id").as_string();
+    // wheel_left and wheel_right MUST be provided, there is no default value
     try {
       wheel_left = this->get_parameter("wheel_left").as_string();
     } catch (rclcpp::exceptions::UninitializedStaticallyTypedParameterException & error_msg) {
@@ -27,13 +32,32 @@ public:
     }
 
 
+    last_time = this->get_clock()->now();
+
+    joint_state_sub_ = this->create_subscription<sensor_msgs::msg::JointState>("joint_state", 10, std::bind(&odometry::joint_state_cb_, this, std::placeholders::_1));
+
   }
 
 private:
+  builtin_interfaces::msg::Time last_time{};
+
   std::string body_id {"base_footprint"};
   std::string odom_id {"odom"};
   std::string wheel_left {"x"};
   std::string wheel_right {"x"};
+
+  nav_msgs::msg::Odometry()
+
+  void joint_state_cb_(const std::shared_prt<sensor_msgs::msg::JointState> msg)
+  {
+    // JointState includes left and right positions, velocities, and time
+
+    // 1st, fk to get new position
+    auto time_diff{
+      msg->stamp.sec + msg->stamp.nanosec / 10e9 - last_time.sec - last_time.nanosec / 10e9};
+
+
+  }
 };
 
 std::shared_ptr<odometry> my_node = nullptr;
