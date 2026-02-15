4de3633ce444bbdf55ece23bb64143fef5c87595
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-02-14 19:40:53 -0600

    Added more twist tests
    Next steps: Add a twist that maxes out the motor cmd.
    Add a mixed translation and rotation.

--- nuturtle_control/test/turtle_control_test.cpp
+++ nuturtle_control/test/turtle_control_test.cpp
@@ -39,7 +39,7 @@ TEST_CASE("Test zero twist to wheel cmd", "[integration]")
   {
     if ((rclcpp::Clock().now() - last_time) > rclcpp::Duration::from_seconds(1)) {       // Only send a message every three seconds
       last_time = rclcpp::Clock().now();
-      RCLCPP_INFO_STREAM(node->get_logger(), "Publishing zero twist");
+      RCLCPP_DEBUG_STREAM(node->get_logger(), "Publishing zero twist");
 
       cmd_vel_pub_->publish(geometry_msgs::msg::Twist());
     }
@@ -49,6 +49,7 @@ TEST_CASE("Test zero twist to wheel cmd", "[integration]")
     REQUIRE_THAT(received_msg->left_velocity, WithinAbs(0.0, 0.000001));
     REQUIRE_THAT(received_msg->right_velocity, WithinAbs(0.0, 0.000001));
 }
+
 
 std::shared_ptr<nuturtlebot_msgs::msg::WheelCommands> received_msg2;
 
@@ -90,7 +91,7 @@ TEST_CASE("Test positive translation", "[integration]")
   {
     if ((rclcpp::Clock().now() - last_time) > rclcpp::Duration::from_seconds(1)) { // Only send a message every three seconds
       last_time = rclcpp::Clock().now();
-      RCLCPP_INFO_STREAM(node->get_logger(), "Publishing 2nd twist");
+      RCLCPP_DEBUG_STREAM(node->get_logger(), "Publishing 2nd twist");
 
       cmd_vel_pub_->publish(twist_msg);
     }
@@ -99,4 +100,114 @@ TEST_CASE("Test positive translation", "[integration]")
   REQUIRE_THAT(received_msg2->left_velocity, WithinAbs(received_msg2->right_velocity, 0.000001));
   REQUIRE_THAT(received_msg2->right_velocity,
     WithinAbs(static_cast<int>(1 / motor_cmd_per_rad_sec), 0.000001));
+}
+
+
+std::shared_ptr<nuturtlebot_msgs::msg::WheelCommands> received_msg3;
+
+void wheel_cmd_cb3_(std::shared_ptr<nuturtlebot_msgs::msg::WheelCommands> msg)
+{
+  received_msg3 = msg;
+  is_msg_received = true;
+}
+
+TEST_CASE("Test negative translation", "[integration]")
+{
+  auto node = rclcpp::Node::make_shared("integration_test_node");
+
+  is_msg_received = false;
+
+  node->declare_parameter("wheel_radius", 0.033);
+  node->declare_parameter("motor_cmd_max", 256);
+  node->declare_parameter("motor_cmd_per_rad_sec", 0.024);
+
+  auto wheel_radius = node->get_parameter("wheel_radius").as_double();
+  auto motor_cmd_max = node->get_parameter("motor_cmd_max").as_int();
+  auto motor_cmd_per_rad_sec = node->get_parameter("motor_cmd_per_rad_sec").as_double();
+
+  auto cmd_vel_pub_ = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto wheel_cmd_sub_ =
+    node->create_subscription<nuturtlebot_msgs::msg::WheelCommands>("wheel_cmd", 10,
+                                                                      wheel_cmd_cb3_);
+
+  auto twist_msg{geometry_msgs::msg::Twist()};
+  twist_msg.linear.x = -1.0 * wheel_radius; // 1 radian rotation to make other calculations easier
+
+  rclcpp::Time start_time = rclcpp::Clock().now();
+  rclcpp::Time last_time = rclcpp::Clock().now();
+  while (
+    rclcpp::ok() &&
+    ((rclcpp::Clock().now() - start_time) < rclcpp::Duration::from_seconds(TEST_DURATION)) &&
+    !is_msg_received)
+  {
+    if ((rclcpp::Clock().now() - last_time) > rclcpp::Duration::from_seconds(1)) { // Only send a message every three seconds
+      last_time = rclcpp::Clock().now();
+      RCLCPP_DEBUG_STREAM(node->get_logger(), "Publishing 3rd twist");
+
+      cmd_vel_pub_->publish(twist_msg);
+    }
+    rclcpp::spin_some(node);
+  }
+
+
+  REQUIRE_THAT(received_msg3->left_velocity, WithinAbs(received_msg3->right_velocity, 0.000001));
+  REQUIRE_THAT(received_msg3->right_velocity,
+               WithinAbs(static_cast<int>(-1 / motor_cmd_per_rad_sec), 0.000001));
+}
+
+
+std::shared_ptr<nuturtlebot_msgs::msg::WheelCommands> received_msg4;
+
+void wheel_cmd_cb4_(std::shared_ptr<nuturtlebot_msgs::msg::WheelCommands> msg)
+{
+  received_msg4 = msg;
+  is_msg_received = true;
+}
+
+TEST_CASE("Test positive rotation", "[integration]")
+{
+  auto node = rclcpp::Node::make_shared("integration_test_node");
+
+  is_msg_received = false;
+
+  node->declare_parameter("wheel_radius", 0.033);
+  node->declare_parameter("motor_cmd_max", 256);
+  node->declare_parameter("motor_cmd_per_rad_sec", 0.024);
+  node->declare_parameter("track_width", 0.16);
+
+  auto wheel_radius = node->get_parameter("wheel_radius").as_double();
+  auto motor_cmd_max = node->get_parameter("motor_cmd_max").as_int();
+  auto motor_cmd_per_rad_sec = node->get_parameter("motor_cmd_per_rad_sec").as_double();
+  auto track_width = node->get_parameter("track_width").as_double();
+
+  auto cmd_vel_pub_ = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  auto wheel_cmd_sub_ =
+    node->create_subscription<nuturtlebot_msgs::msg::WheelCommands>("wheel_cmd", 10,
+                                                                      wheel_cmd_cb4_);
+
+  auto twist_msg{geometry_msgs::msg::Twist()};
+  twist_msg.angular.z = 1; // 1 radian rotation to make other calculations easier
+
+  rclcpp::Time start_time = rclcpp::Clock().now();
+  rclcpp::Time last_time = rclcpp::Clock().now();
+  while (
+    rclcpp::ok() &&
+    ((rclcpp::Clock().now() - start_time) < rclcpp::Duration::from_seconds(TEST_DURATION)) &&
+    !is_msg_received)
+  {
+    if ((rclcpp::Clock().now() - last_time) > rclcpp::Duration::from_seconds(1)) { // Only send a message every three seconds
+      last_time = rclcpp::Clock().now();
+      RCLCPP_DEBUG_STREAM(node->get_logger(), "Publishing 4th twist");
+
+      cmd_vel_pub_->publish(twist_msg);
+    }
+    rclcpp::spin_some(node);
+  }
+
+  rclcpp::spin_some(node);
+  REQUIRE_THAT(received_msg4->left_velocity,
+    WithinAbs(static_cast<int>(-1 * track_width / 2 / wheel_radius / motor_cmd_per_rad_sec),
+    0.000001));
+  REQUIRE_THAT(received_msg4->right_velocity,
+    WithinAbs(static_cast<int>(track_width / 2 / wheel_radius / motor_cmd_per_rad_sec), 0.000001));
 }
