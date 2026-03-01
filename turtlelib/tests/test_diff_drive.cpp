97e10857491f450782986d2dbb7fe5f21ef40756
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-03-01 04:52:47 -0600

    Changed all get_wheels, .left, .right in test_diff_drive

--- turtlelib/tests/test_diff_drive.cpp
+++ turtlelib/tests/test_diff_drive.cpp
@@ -20,8 +20,8 @@ TEST_CASE("Test fk", "DiffDrive::fk")
   REQUIRE(dd.get_transform().translation().x == 0);
   REQUIRE(dd.get_transform().translation().y == 0);
   REQUIRE(dd.get_transform().rotation() == 0);
-  REQUIRE(dd.get_wheels().left == 0);
-  REQUIRE(dd.get_wheels().right == 0);
+  REQUIRE(dd.phi().l() == 0);
+  REQUIRE(dd.phi().r() == 0);
 
   // small equal rotation on both wheels should result in x translation of that rotation amount
   auto rot {pi / 4.0};
@@ -29,24 +29,24 @@ TEST_CASE("Test fk", "DiffDrive::fk")
   REQUIRE(dd.get_transform().translation().x == rot);
   REQUIRE(dd.get_transform().translation().y == 0);
   REQUIRE(dd.get_transform().rotation() == 0);
-  REQUIRE(dd.get_wheels().left == rot);
-  REQUIRE(dd.get_wheels().right == rot);
+  REQUIRE(dd.phi().l() == rot);
+  REQUIRE(dd.phi().r() == rot);
 
   // Returning to the previous position should return state to original
   dd.fk(Wheels(0, 0));
   REQUIRE(dd.get_transform().translation().x == 0.0);
   REQUIRE(dd.get_transform().translation().y == 0.0);
   REQUIRE(dd.get_transform().rotation() == 0.0);
-  REQUIRE(dd.get_wheels().left == 0.0);
-  REQUIRE(dd.get_wheels().right == 0.0);
+  REQUIRE(dd.phi().l() == 0.0);
+  REQUIRE(dd.phi().r() == 0.0);
 
   // Wheels rotation equal distances in opposite direction should spin with no translation
   dd.fk(Wheels(-rot, rot));
   REQUIRE(dd.get_transform().translation().x == 0.0);
   REQUIRE(dd.get_transform().translation().y == 0.0);
   REQUIRE(dd.get_transform().rotation() == rot);
-  REQUIRE(dd.get_wheels().left == -rot);
-  REQUIRE(dd.get_wheels().right == rot);
+  REQUIRE(dd.phi().l() == -rot);
+  REQUIRE(dd.phi().r() == rot);
 
   // Back it up
   rot = 0.0;
@@ -54,24 +54,24 @@ TEST_CASE("Test fk", "DiffDrive::fk")
   REQUIRE(dd.get_transform().translation().x == 0.0);
   REQUIRE(dd.get_transform().translation().y == 0.0);
   REQUIRE(dd.get_transform().rotation() == 0.0);
-  REQUIRE(dd.get_wheels().left == 0.0);
-  REQUIRE(dd.get_wheels().right == 0.0);
+  REQUIRE(dd.phi().l() == 0.0);
+  REQUIRE(dd.phi().r() == 0.0);
 
   // Rotate only one wheel should result in an x, y, and theta change (multiple iterations so I get to a location with easy numbers)
   DiffDrive dd2{2.0, 1.0};  // Wheels have circumference 2*pi
 
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
-  dd2.fk(Wheels(0, dd2.get_wheels().right + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
+  dd2.fk(Wheels(0, dd2.phi().r() + pi / 4));
 
 
-  REQUIRE(dd2.get_wheels().left == 0);
-  REQUIRE(dd2.get_wheels().right == normalize_angle(2 * pi));
+  REQUIRE(dd2.phi().l() == 0);
+  REQUIRE(dd2.phi().r() == normalize_angle(2 * pi));
   REQUIRE_THAT(dd2.get_transform().translation().x, WithinAbs(0, 0.00001));
   REQUIRE_THAT(dd2.get_transform().translation().y, WithinAbs(2, 0.00001));
   REQUIRE_THAT(dd2.get_transform().rotation(), WithinAbs(normalize_angle(pi), 0.00001));
@@ -82,29 +82,29 @@ TEST_CASE("Test ik", "DiffDrive::ik")
   DiffDrive dd{2.0, 1.0};
 
   auto speeds{DiffDrive{2.0, 1.0}.ik({0.0, 1, 0.0})}; // Pure translation +
-  REQUIRE(dd.ik({0.0, 1, 0.0}).left == 1);
-  REQUIRE(speeds.right == 1);
+  REQUIRE(dd.ik({0.0, 1, 0.0}).l() == 1);
+  REQUIRE(speeds.r() == 1);
 
   speeds = DiffDrive{2.0, 1.0}.ik({0.0, -1.0, 0.0}); // Pure translation -
-  REQUIRE(speeds.left == -1);
-  REQUIRE(speeds.right == -1);
+  REQUIRE(speeds.l() == -1);
+  REQUIRE(speeds.r() == -1);
 
   speeds = DiffDrive{2.0, 1.0}.ik({1.0, 0.0, 0.0});
-  REQUIRE(speeds.left == -1);
-  REQUIRE(speeds.right == 1);
+  REQUIRE(speeds.l() == -1);
+  REQUIRE(speeds.r() == 1);
 
   speeds = DiffDrive{2.0, 1.0}.ik({-1.0, 0.0, 0.0}); // Pure rotation -
-  REQUIRE(speeds.left == 1);
-  REQUIRE(speeds.right == -1);
+  REQUIRE(speeds.l() == 1);
+  REQUIRE(speeds.r() == -1);
 
   speeds = DiffDrive{2.0, 1.0}.ik({pi / 2, pi / 2, 0.0});
-  REQUIRE(speeds.left == 0);
-  REQUIRE(speeds.right == pi);
+  REQUIRE(speeds.l() == 0);
+  REQUIRE(speeds.r() == pi);
 
   // Twist2D body_tw4{-pi / 2, -pi / 2, 0.0};
   speeds = DiffDrive{2.0, 1.0}.ik({-pi / 2, -pi / 2, 0.0});
-  REQUIRE(speeds.left == 0);
-  REQUIRE(speeds.right == -pi);
+  REQUIRE(speeds.l() == 0);
+  REQUIRE(speeds.r() == -pi);
 
   REQUIRE_THROWS_AS(dd.ik({1, 1, 1}), std::logic_error);
 }
